fcef824ae8d9b4e519a4458f429c1c824b544762
Nicolas <ngranato11@gmail.com>
2026-09-17 10:13:43 -0300

    ✨feat: enhance DisposableAuditDatabase connection handling and update schema for SPED documents

--- src/database/queries/schema.sql
+++ src/database/queries/schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS xml_documents (
 
 CREATE TABLE IF NOT EXISTS sped_documents (
     access_key VARCHAR(44),
+    total_value BIGINT,
     sped_type VARCHAR(12),
     situation_code VARCHAR(2),
-    total_value BIGINT,
     PRIMARY KEY (access_key, sped_type)
 );
