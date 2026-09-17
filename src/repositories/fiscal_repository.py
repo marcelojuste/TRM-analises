a8f93ac0fd645567630d523222e3dab732848ad2
Nicolas <ngranato11@gmail.com>
2026-09-17 10:14:27 -0300

    ✨feat: implement FiscalRepository for managing XML and SPED document batches

---
+++ src/repositories/fiscal_repository.py
@@ -0,0 +1,39 @@
+from typing import List, Tuple
+
+from src.database.database import DisposableAuditDatabase
+
+class FiscalRepository:
+
+    def __init__(self):
+        self.xml_batch: List[Tuple] = []
+        self.sped_batch: List[Tuple] = []
+        self._conn = None
+
+    def set_conn(self, database: DisposableAuditDatabase):
+        self._conn = database.get_conn()
+
+    def _flush_xmls(self) -> None:
+        if not self._conn:
+            ConnectionError("Conexão com o banco não encontrada")
+            
+        if self.xml_batch:
+            self._conn.executemany(
+                "INSERT INTO xml_documents VALUES (?, ?)", 
+                self.xml_batch
+            )
+            self.xml_batch.clear()
+
+    def _flush_speds(self) -> None:
+        if not self._conn:
+            ConnectionError("Conexão com o banco não encontrada")
+
+        if self.sped_batch:
+            self._conn.executemany(
+                "INSERT INTO sped_documents Values(?, ?, ?, ?)",
+                self.sped_batch
+            )
+            self.sped_batch.clear()
+
+    def flush_batchs(self) -> None:
+        self._flush_xmls()
+        self._flush_speds()
